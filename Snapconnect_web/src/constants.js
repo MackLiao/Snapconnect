785e898e66c2c93f50bf282c3f2d2f66942571d2
Mack Liao <g.liao@wustl.edu>
2024-12-08 19:16:36 -0600

    Reformat sensitive files.

--- Snapconnect_web/src/constants.js
+++ Snapconnect_web/src/constants.js
@@ -1,6 +1,5 @@
 export const TOKEN_KEY = "token";
-//export const BASE_URL = "https://around-dot-around-179500.uc.r.appspot.com";
-export const BASE_URL = "https://spheric-rhythm-0305.uc.r.appspot.com";
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const SEARCH_KEY = {
   all: 0,
   keyword: 1,
